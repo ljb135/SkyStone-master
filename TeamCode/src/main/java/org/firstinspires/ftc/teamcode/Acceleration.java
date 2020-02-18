0001a82e9a40d62252b8c67d946df1a3b4dfc234
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2020-02-18 17:11:32 -0500

    Working Multithreading and Acceleration
    Signed-off-by: KaushikTheProgrammer <kaushikpprakash@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Acceleration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Acceleration.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -16,29 +17,52 @@ public class Acceleration extends LinearOpMode {
     private DcMotor FLDrive = null;
     private DcMotor BRDrive = null;
     private DcMotor BLDrive = null;
+    private int initialValue = 0;
+    PIDController drivePid;
+    private ModernRoboticsI2cGyro modernRoboticsI2cGyro;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        modernRoboticsI2cGyro = hardwareMap.get(ModernRoboticsI2cGyro.class, "gyro");
         FRDrive  = hardwareMap.get(DcMotor.class, "front_right");
         FLDrive = hardwareMap.get(DcMotor.class, "front_left");
         BRDrive  = hardwareMap.get(DcMotor.class, "back_right");
         BLDrive  = hardwareMap.get(DcMotor.class, "back_left");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
 
         FRDrive.setDirection(DcMotor.Direction.REVERSE);
         FLDrive.setDirection(DcMotor.Direction.FORWARD);
         BRDrive.setDirection(DcMotor.Direction.REVERSE);
         BLDrive.setDirection(DcMotor.Direction.FORWARD);
 
+        drivePid = new PIDController(0.01, 0, 0);
+
+        telemetry.log().add("Gyro Calibrating. Do Not Move!");
+        modernRoboticsI2cGyro.calibrate();
+
+        // Wait until the gyro calibration is complete
+        runtime.reset();
+        while (!isStopRequested() && modernRoboticsI2cGyro.isCalibrating())  {
+            telemetry.addData("calibrating", "%s", Math.round(runtime.seconds()) % 2 == 0 ? "|.." : "..|");
+            telemetry.update();
+            sleep(50);
+        }
+
+        telemetry.log().clear();
+        telemetry.log().add("Gyro Calibrated. Press Start.");
+        telemetry.clear();
+        telemetry.update();
+
+
+
         FRDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         FLDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BLDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BRDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         int targetPosition = 5000;
+        int robotAngle = 0;
 
         FRDrive.setTargetPosition(targetPosition);
         FLDrive.setTargetPosition(targetPosition);
@@ -47,29 +71,57 @@ public class Acceleration extends LinearOpMode {
 
 
         double propAccel = 0.2;
-        double propDecel = 0.7;
-        double accelTarget = targetPosition * propAccel;
-        double decelTarget = targetPosition - (targetPosition * propDecel);
-        telemetry.addData("Accel Target", accelTarget);
-        telemetry.addData("Decel Target", decelTarget);
-        telemetry.update();
+        double propDecel = 0.5;
+        double accelPow = 2;
+        double decelPow = 1.3;
 
         waitForStart();
+        modernRoboticsI2cGyro.resetZAxisIntegrator();
+
+        accelMove(robotAngle,targetPosition, propAccel, propDecel, accelPow, decelPow);
+
+    }
+
+    void accelMove(int desiredAngle, int targetPosition, double propAccel, double propDecel, double accelPow, double decelPow) {
+        drivePid.reset();
+        drivePid.setSetpoint(desiredAngle);
+        drivePid.setInputRange(-359, 359);
+        drivePid.setTolerance(1);
+
+        drivePid.enable();
+
+        double accelTarget = targetPosition * propAccel;
+        double decelTarget = targetPosition - (targetPosition * propDecel);
+
+
+        double remainingDist = (accelTarget - FRDrive.getCurrentPosition()) / accelTarget;
+        double motorPower = 0;
+        double leftPower = 0;
+        double rightPower = 0;
+
+        int robotAngle = modernRoboticsI2cGyro.getIntegratedZValue();
+        double correction = drivePid.performPID(robotAngle);
+
 
         // Acceleration
-        double remainingDist = (accelTarget - FRDrive.getCurrentPosition()) / accelTarget;
-        double motorPower = Range.clip(Math.pow(remainingDist, 2), 0.1, 1.0);
-
         while(opModeIsActive() && remainingDist > 0) {
-            motorPower = Range.clip(Math.pow(remainingDist, 2), 0.1, 1.0);
-            FRDrive.setPower(1.1 - motorPower);
-            FLDrive.setPower(1.1 - motorPower);
-            BRDrive.setPower(1.1 - motorPower);
-            BLDrive.setPower(1.1 - motorPower);
+            motorPower = Range.clip(Math.pow(remainingDist, accelPow), 0.1, 1.0);
+            if(targetPosition > 0) {
+                leftPower = motorPower + correction;
+                rightPower = motorPower - correction;
+            }  else {
+                leftPower = motorPower - correction;
+                rightPower = motorPower + correction;
+            }
+
+            FRDrive.setPower(1.1 - rightPower);
+            FLDrive.setPower(1.1 - leftPower);
+            BRDrive.setPower(1.1 - rightPower);
+            BLDrive.setPower(1.1 - leftPower);
             remainingDist = (accelTarget - FRDrive.getCurrentPosition()) / accelTarget;
+            telemetry.addLine("Status: Accelerating");
             telemetry.addData("Position", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getCurrentPosition(), (float)FLDrive.getCurrentPosition(), (float)BRDrive.getCurrentPosition(), (float)BLDrive.getCurrentPosition());
             telemetry.addData("Power", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getPower(), (float)FLDrive.getPower(), (float)BRDrive.getPower(), (float)BLDrive.getPower());
-            telemetry.update();
             telemetry.update();
         }
 
@@ -79,9 +131,9 @@ public class Acceleration extends LinearOpMode {
             FLDrive.setPower(1.1 - motorPower);
             BRDrive.setPower(1.1 - motorPower);
             BLDrive.setPower(1.1 - motorPower);
+            telemetry.addLine("Status: Constant Vel");
             telemetry.addData("Position", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getCurrentPosition(), (float)FLDrive.getCurrentPosition(), (float)BRDrive.getCurrentPosition(), (float)BLDrive.getCurrentPosition());
             telemetry.addData("Power", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getPower(), (float)FLDrive.getPower(), (float)BRDrive.getPower(), (float)BLDrive.getPower());
-            telemetry.update();
             telemetry.update();
         }
 
@@ -90,19 +142,20 @@ public class Acceleration extends LinearOpMode {
         telemetry.update();
 
         // Deceleration
-        while(opModeIsActive() && ((FLDrive.isBusy() && FRDrive.isBusy() && BLDrive.isBusy() && BRDrive.isBusy()) || remainingDist > 0)) {
-            motorPower = Range.clip(Math.pow(remainingDist, 1.3), 0.15, 1.0);
+        while(opModeIsActive() && remainingDist > 0) {
+            motorPower = Range.clip(Math.pow(remainingDist, decelPow), 0.2, 1.0);
             FRDrive.setPower(motorPower);
             FLDrive.setPower(motorPower);
             BRDrive.setPower(motorPower);
             BLDrive.setPower(motorPower);
             remainingDist = (targetPosition - FRDrive.getCurrentPosition()) / (targetPosition - decelTarget);
-
+            telemetry.addLine("Status: Decelerating");
             telemetry.addData("Position", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getCurrentPosition(), (float)FLDrive.getCurrentPosition(), (float)BRDrive.getCurrentPosition(), (float)BLDrive.getCurrentPosition());
             telemetry.addData("Power", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getPower(), (float)FLDrive.getPower(), (float)BRDrive.getPower(), (float)BLDrive.getPower());
             telemetry.update();
         }
 
+        telemetry.addLine("Status: Move Complete");
         FRDrive.setPower(0);
         FLDrive.setPower(0);
         BRDrive.setPower(0);
@@ -115,3 +168,4 @@ public class Acceleration extends LinearOpMode {
     }
 
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Multithreading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Multithreading.java
@@ -15,6 +15,7 @@ public class Multithreading extends LinearOpMode {
     private DcMotor FLDrive = null;
     private DcMotor BRDrive = null;
     private DcMotor BLDrive = null;
+    private DcMotor lift = null;
 
 
     @Override
@@ -22,58 +23,76 @@ public class Multithreading extends LinearOpMode {
 
         FRDrive  = hardwareMap.get(DcMotor.class, "front_right");
         FLDrive = hardwareMap.get(DcMotor.class, "front_left");
-//        BRDrive  = hardwareMap.get(DcMotor.class, "back_right");
-//        BLDrive  = hardwareMap.get(DcMotor.class, "back_left");
+        BRDrive  = hardwareMap.get(DcMotor.class, "back_right");
+        BLDrive  = hardwareMap.get(DcMotor.class, "back_left");
+        lift = hardwareMap.get(DcMotor.class, "lift");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-
         FRDrive.setDirection(DcMotor.Direction.REVERSE);
         FLDrive.setDirection(DcMotor.Direction.FORWARD);
-//        BRDrive.setDirection(DcMotor.Direction.REVERSE);
-//        BLDrive.setDirection(DcMotor.Direction.FORWARD);
+        BRDrive.setDirection(DcMotor.Direction.REVERSE);
+        BLDrive.setDirection(DcMotor.Direction.FORWARD);
+        lift.setDirection(DcMotor.Direction.FORWARD);
+
 
         FRDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         FLDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        final double power = 0.6;
+        final int targetPosition = 5000;
+        final int thresholdPosition = 3000;
 
         Thread thread1 = new Thread(){
             public void run(){
-                telemetry.addData("FR Moving",1 );
-                telemetry.update();
-                FRDrive.setTargetPosition(2000);
-                while(FRDrive.getPower() != power){
-                    FRDrive.setPower(power);
-                }
-
-
-            }
-        };
-
-        Thread thread2 = new Thread(){
-            public void run(){
-                telemetry.addData("Before sleep",1 );
-                telemetry.update();
-                try {
-                    sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 telemetry.addData("FL Moving",1 );
                 telemetry.update();
-                FLDrive.setTargetPosition(2000);
-                while(FLDrive.getPower() != power){
-                    FLDrive.setPower(power);
+                lift.setTargetPosition(1500);
+                lift.setPower(0.5);
+                while(lift.isBusy()) {
+                    telemetry.addLine("Lift is elevating");
+                    telemetry.update();
                 }
-
+                lift.setTargetPosition(0);
+                lift.setPower(0.5);
+                while(lift.isBusy()) {
+                    telemetry.addLine("Lift is lowering");
+                    telemetry.update();
+                }
             }
         };
 
-        thread1.run();
-        thread2.run();
+        waitForStart();
 
+        FLDrive.setTargetPosition(targetPosition);
+        BLDrive.setTargetPosition(targetPosition);
+        BRDrive.setTargetPosition(targetPosition);
+        FRDrive.setTargetPosition(targetPosition);
+
+        FLDrive.setPower(0.8);
+        FRDrive.setPower(0.8);
+        BLDrive.setPower(0.8);
+        BRDrive.setPower(0.8);
+
+        boolean startThread = false;
+
+        while (opModeIsActive() && (FLDrive.isBusy() && FRDrive.isBusy() && BLDrive.isBusy() && BRDrive.isBusy())) {
+
+            if(!startThread && FLDrive.getCurrentPosition() > thresholdPosition) {
+                startThread = true;
+                thread1.start();
+            }
+
+            telemetry.addData("Position", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getCurrentPosition(), (float)FLDrive.getCurrentPosition(), (float)BRDrive.getCurrentPosition(), (float)BLDrive.getCurrentPosition());
+            telemetry.addData("Target Position", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getTargetPosition(), (float)FLDrive.getTargetPosition(), (float)BRDrive.getTargetPosition(), (float)BLDrive.getTargetPosition());
+            telemetry.addData("Power", "FR: (%.2f) FL: (%.2f) BR: (%.2f) BL: (%.2f)", (float)FRDrive.getPower(), (float)FLDrive.getPower(), (float)BRDrive.getPower(), (float)BLDrive.getPower());
+            telemetry.update();
+        }
+
+        thread1.join();
 
     }
-
 }
+
